c762a0d625cd28bb7c6c0da6a042e0177dc4b53f
Błażej Bartoszewski <blazejbart@gmail.com>
2024-01-16 01:59:33 +0100

    part 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from './Profile/user.json/user.json';
 import data from './Statistics/data.json/data.json';
 import friends from './FriendList/friends.json/friends.json';
+import transactions from './TransactionHistory/transactions.json/transactions.json';
 
 export const App = () => {
   return (
@@ -27,6 +29,7 @@ export const App = () => {
       />
       <Statistics title="UPLOAD STATS" stats={data} />
       <FriendList friends={friends} />{' '}
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
